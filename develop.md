38488e299fe4bdd88bff35a940066e27b4caf2c1
Tiago Neves <rykarumstuler@gmail.com>
2024-05-19 00:23:42 -0300

    Get Objects Auto

---
+++ develop.md
@@ -0,0 +1,7 @@
+# Referências
+https://developer.salesforce.com/docs/component-library/bundle/lightning:recordEditForm/documentation
+
+https://developer.salesforce.com/docs/atlas.en-us.apexref.meta/apexref/apex_methods_system_schema.htm#apex_System_Schema_getGlobalDescribe
+
+https://developer.salesforce.com/docs/atlas.en-us.lightning.meta/lightning/controllers_server_actions_call.htm
+
--- force-app/main/default/aura/CreateRuleButton/CreateRuleButtonController.js
+++ force-app/main/default/aura/CreateRuleButton/CreateRuleButtonController.js
@@ -1,5 +1,56 @@
 ({
-    myAction : function(component, event, helper) {
+    init : function(component, event, helper) {
+        // TODO: Check if can we use Schema.getGlobalDescribe() to set in attribute v.options
+        const getAllObjectsAction = component.get('c.getAllObjects');
 
+        getAllObjectsAction.setCallback(this, function(response) {
+            const state = response.getState();
+
+            switch (state) {
+                case 'SUCCESS':
+                    console.log('SUCCESS');
+                    const returnValue = JSON.parse( response.getReturnValue() );
+                    console.log('returnValue');
+                    console.log(returnValue);
+                    component.set('v.optionsObjects', returnValue);
+                    break;
+                
+                case 'INCOMPLETE':
+                    console.warn('INCOMPLETE');
+                    console.warn(JSON.parse(JSON.stringify(response)));
+                    break;
+
+                case 'ERROR':
+                    const errors = response.getError();
+                    if (errors) {
+                        if (errors[0] && errors[0].message) {
+                            console.warn("Error message: " + 
+                                    errors[0].message);
+                        }
+                    } else {
+                        console.log("Unknown error");
+                    }
+                    break;
+                
+                default:
+                    console.warn('SOMETHING WRONG');
+                    break;
+            }
+        });
+
+        $A.enqueueAction(getAllObjectsAction);
+    },
+
+    // Methods FORM
+    handleSubmit: function(component, event, helper) {
+        event.preventDefault();       // stop the form from submitting
+        var fields = event.getParam('fields');
+        fields.Object__c = component.get('v.objectSelected');
+        component.find('ruleRecordEditForm').submit(fields);
     }
+    // handleSuccess: function(component, event) {
+    //     var updatedRecord = JSON.parse(JSON.stringify(event.getParams()));
+    //     console.log('onsuccess: ', updatedRecord.id);
+    //     // Navigate to record here.
+    // }
 })
